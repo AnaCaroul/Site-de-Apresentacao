cef15f64eeacc3e5a6b0f23d1afcb764b67f315b
Ana Carolina <anacarolinaalvesramos03@gmail.com>
2022-10-29 23:54:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function aindaNaoSei(){
+    const valores = {
+       name: document.getElementById("name").value , 
+       email: document.getElementById ("Email").value ,
+       message: document.getElementById ("Message").value 
+    };
+   
+    console.log(valores)
+   };
